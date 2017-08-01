3f3fbcd2ab4210cdc0a2c991f3a608ee8b017143
Saulo Vallory <me@saulovallory.com>
2017-08-01 17:46:28 -0300

    Use global scoped symbols instead of local symbols

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,5 +1,6 @@
-export const PK = Symbol('PK');
-export const TABLE = Symbol('TABLE');
-export const TABLE_NAME = Symbol('TABLE_NAME');
-export const FIELDS = Symbol('FIELDS');
-export const DTO = Symbol('DTO');
+export const TABLE = Symbol.for('cashfarm.store.table');
+export const PK = Symbol.for('cashfarm.store.table.pk');
+export const TABLE_NAME = Symbol.for('cashfarm.store.table.name');
+export const FIELDS = Symbol.for('cashfarm.store.table.fields');
+export const DTO = Symbol.for('cashfarm.store.dto');
+export const MAPPER = Symbol.for('cashfarm.store.dto.mapper');
